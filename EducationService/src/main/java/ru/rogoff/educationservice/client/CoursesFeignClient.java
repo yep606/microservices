246c0a537a57fbcacccbf6c67b03c4f246f3ccec
Ilya Rogov <ilya.rogov.vladislav@gmail.com>
2024-11-21 18:31:57 +0300

    Add Retry policy on calls

--- EducationService/src/main/java/ru/rogoff/educationservice/client/CoursesFeignClient.java
+++ EducationService/src/main/java/ru/rogoff/educationservice/client/CoursesFeignClient.java
@@ -1,6 +1,7 @@
 package ru.rogoff.educationservice.client;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +14,7 @@ import java.util.UUID;
 @FeignClient(name = "COURSE-SERVICE")
 public interface CoursesFeignClient {
     @GetMapping("/courses/{cvUuid}")
+    @Retry(name = "course-service", fallbackMethod = "getFallback")
     @CircuitBreaker(name = "course-service", fallbackMethod = "getFallback")
     List<CourseResponseDto> get(@PathVariable UUID cvUuid);
 
