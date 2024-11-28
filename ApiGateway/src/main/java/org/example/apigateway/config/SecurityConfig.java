a1c951f6ca388281fd0ed064e561b974e4d7efa8
Ilya Rogov <ilya.rogov.vladislav@gmail.com>
2024-11-29 01:58:15 +0300

    Add keycloak as external auth server

---
+++ ApiGateway/src/main/java/org/example/apigateway/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package org.example.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverter;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http
+                .authorizeExchange(exchanges -> exchanges
+                        .anyExchange().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter()))
+                );
+        return http.build();
+    }
+
+    private ReactiveJwtAuthenticationConverter jwtAuthenticationConverter() {
+        return new ReactiveJwtAuthenticationConverter();
+    }
+}
